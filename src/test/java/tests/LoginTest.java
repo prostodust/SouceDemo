8193e914a5dd62041bc37ea9e17210c49103a0a0
Oleg <prostodust@gmail.com>
2021-07-05 09:01:43 +0300

    Code refactoring

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import utils.PropertyReader;
 
@@ -13,8 +15,7 @@ public class LoginTest extends BaseTest implements ITestConstants {
     public void enterCorrectUsernameAndPasswordTest() {
         loginPage.openPage(SAUCE_DEMO_BASE_URL)
                 .waitForElementBotLogoLocated(10)
-//                .login(STANDARD_USER_LOGIN, STANDARD_USER_PASSWORD)
-                .login(System.getenv().getOrDefault("user", PropertyReader.getProperty("username")), System.getenv().getOrDefault("pass", PropertyReader.getProperty("password")))
+                .login(STANDARD_USER_LOGIN, STANDARD_USER_PASSWORD)
                 .waitForPageOpened();
         Assert.assertEquals(productsPage.getProductCompleteText(), "PRODUCTS");
     }
@@ -44,15 +45,51 @@ public class LoginTest extends BaseTest implements ITestConstants {
     }
 
     /**
-     * Passing parameters from the Maven command line inside the test
      * Invalid value in the "name" and "password" fields
      */
-    @Test(description = "Empty value in the \"name\" and \"password\" fields")
-    public void enterEmptyUsernameAndPasswordTest() {
+    @Test(description = "Invalid value in the \"name\" and \"password\" fields")
+    public void enterInvalidUsernameAndPasswordTest() {
         loginPage.openPage(SAUCE_DEMO_BASE_URL)
                 .waitForElementBotLogoLocated(20)
-                .login(System.getProperty("username", "standard"), System.getProperty("password", "secret"))
+                .login("standard", "secret")
                 .waitForPageLoaded();
         Assert.assertEquals(loginPage.getErrorMessageText(), "Epic sadface: Username and password do not match any user in this service");
+    }
+
+    /**
+     * Passing parameters from the file config.properties inside the test
+     */
+    @Test(description = "Passing parameters from the file config.properties inside the test", groups = {"smoke"})
+    public void enterUsernameAndPasswordFromConfigTest() {
+        loginPage.openPage(SAUCE_DEMO_BASE_URL)
+                .waitForElementBotLogoLocated(10)
+                .login(System.getenv().getOrDefault("userFromConfig", PropertyReader.getProperty("user")), System.getenv().getOrDefault("passFromConfig", PropertyReader.getProperty("pass")))
+                .waitForPageOpened();
+        Assert.assertEquals(productsPage.getProductCompleteText(), "PRODUCTS");
+    }
+
+    /**
+     * Passing parameters from the Maven command line inside the test
+     */
+    @Test(description = "Passing parameters \"name\" and \"password\" from the Maven command line inside the test")
+    public void enterUsernameAndPasswordFromMavenTest() {
+        loginPage.openPage(SAUCE_DEMO_BASE_URL)
+                .waitForElementBotLogoLocated(20)
+                .login(System.getProperty("userFromMaven", "standard_user"), System.getProperty("passFromMaven", "secret_sauce"))
+                .waitForPageLoaded();
+        Assert.assertEquals(productsPage.getProductCompleteText(), "PRODUCTS");
+    }
+
+    /**
+     * Passing parameters TestNG from the file soucedemo.xml and inside the test
+     */
+    @Parameters({"userFromXML", "passFromXML"})
+    @Test(description = "Passing parameters \"name\" and \"password\" from the TestNG inside the test")
+    public void enterUsernameAndPasswordFromTestNGTest(@Optional(STANDARD_USER_LOGIN) String p1, @Optional(STANDARD_USER_PASSWORD) String p2) {
+        loginPage.openPage(SAUCE_DEMO_BASE_URL)
+                .waitForElementBotLogoLocated(20)
+                .login(p1, p2)
+                .waitForPageLoaded();
+        Assert.assertEquals(productsPage.getProductCompleteText(), "PRODUCTS");
     }
 }
--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,22 +1,19 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ProductsTest extends BaseTest implements ITestConstants {
     /**
      * Changing the text on the button
      */
-    @Parameters({"username", "password"})
     @Test(description = "Changing the text on the button")
-    public void changingButtonNameTest(@Optional(STANDARD_USER_LOGIN) String p1, @Optional(STANDARD_USER_PASSWORD) String p2) {
+    public void changingButtonNameTest() {
         String productName = "Sauce Labs Bolt T-Shirt";
         loginPage
                 .openPage(SAUCE_DEMO_BASE_URL)
                 .waitForElementBotLogoLocated(10)
-                .login(p1, p2)
+                .login(STANDARD_USER_LOGIN, STANDARD_USER_PASSWORD)
                 .waitForPageOpened()
                 .addProductToCart(productName);
         Assert.assertEquals(productsPage.getAddProductToCartButtonText(productName), "REMOVE");
